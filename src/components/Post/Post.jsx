81abeb76c203bb3d19a3590bd4f7a9eb9cf54101
your_name <marina.skorik@list.ru>
2023-11-28 21:15:38 +0300

    Чуть чуть дизайна

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,6 +3,9 @@ import './PostStyles.css'
 import { useGetAllCommentsQuery } from "../../query/posts";
 import { getTodos } from "../../api";
 import { useEffect } from 'react';
+import { IoMdHeartEmpty } from "react-icons/io";
+import { MdDeleteOutline } from "react-icons/md";
+import { MdOutlineEdit } from "react-icons/md";
 
 const Post = () => {
 
@@ -24,11 +27,28 @@ const Post = () => {
     <>
       {isLoading ? null : data.map((post) => {
         return (
-          <div key={post.id} className="post">
-            <p className="post_author" >Leanne Graham</p>
-            <h3 className="post_title">{post.title}</h3>
-            <p className="post_text">{post.body}</p>
-            <button className="post_button-comments">Комментарии</button>
+          <div className="post">
+            <div className="post_side-bar">
+              <div className="post_side-like">
+                <IoMdHeartEmpty />
+              </div>
+              <div className="post_side-like">
+                <MdDeleteOutline />
+              </div>
+              <div className="post_side-like">
+                <MdOutlineEdit />
+              </div>
+
+            </div>
+            <div key={post.id} className="post_main">
+              <div className="post_header">
+                <p className="post_author" >Leanne Graham</p>
+                <h3 className="post_title">{post.title}</h3>
+                <p className="post_text">{post.body}</p>
+              </div>
+              <button className="post_button-comments">Комментарии</button>
+            </div>
+
           </div>
         )
 
